4811e78dd3866230a3140aa9c863913780dcaa05
Dark10CRK <lirenzhao@gmail.com>
2015-05-23 23:35:37 +0800

    Initial Commit

---
+++ 002/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  002
+//
+//  Created by Dark10CRK on 15/5/23.
+//  Copyright (c) 2015年 Dark10CRK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
